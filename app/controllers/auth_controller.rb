4f5ee678a51304d9da1c00b54ffda54df80a2cb1
Eldar Eminov <em88el@gmail.com>
2024-11-29 12:45:20 +0300

    Update TelegramWebApp

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -4,29 +4,18 @@ class AuthController < ApplicationController
   skip_before_action :verify_authenticity_token, only: :telegram_auth
 
   def telegram_auth
-    Rails.logger.info request.body.read
-    secret_key = OpenSSL::Digest::SHA256.digest(ENV['TELEGRAM_BOT_TOKEN'])
     data = params.to_unsafe_h.except(:controller, :action)
-
-    binding.pry
-    check_string = data.sort.map { |k, v| "#{k}=#{v}" }.join("\n")
-    hmac = OpenSSL::HMAC.hexdigest(OpenSSL::Digest::SHA256.new, secret_key, check_string)
-
-    if hmac == data['hash']
-      user = User.find_or_create_by(tg_id: data['user']['id']) do |u|
-        u.username = data['user']['username']
-        u.first_name = data['user']['first_name']
-        u.last_name = data['user']['last_name']
-        u.photo_url = data['user']['photo_url']
-      end
-
-      # Логиним пользователя
-      session[:user_id] = user.id
-
-      render json: { success: true, user: user }
-    else
-      render json: { success: false, error: 'Invalid hash' }, status: :unauthorized
+    user = User.find_or_create_by(tg_id: data['user']['id']) do |u|
+      u.username   = data['user']['username']
+      u.first_name = data['user']['first_name']
+      u.last_name  = data['user']['last_name']
+      u.photo_url  = data['user']['photo_url']
+      u.email      = generate_email(data['user']['id'])
+      u.password   = Devise.friendly_token[0, 20]
     end
+
+    sign_in(user)
+    render json: { success: true, user: user }
   end
 
   private
