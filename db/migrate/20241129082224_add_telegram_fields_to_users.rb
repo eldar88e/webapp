34c0a5b01146fd7cc70fb8c7f11c8f279d7c3733
Eldar Eminov <em88el@gmail.com>
2024-11-29 11:26:22 +0300

    Add migration for prev commit

---
+++ db/migrate/20241129082224_add_telegram_fields_to_users.rb
@@ -0,0 +1,8 @@
+class AddTelegramFieldsToUsers < ActiveRecord::Migration[7.2]
+  def change
+    add_column :users, :username, :string
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :photo_url, :string
+  end
+end
