82593e94394c4afa47abb3eba120f7d562878eea
Eldar Eminov <em88el@gmail.com>
2024-12-06 15:05:07 +0300

    Update cart -+ btn function

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -9,7 +9,10 @@ class CartItemsController < ApplicationController
     cart_item.quantity += 1 if cart_item.persisted?
     return render turbo_stream: [
       turbo_stream.replace(:cart_logo, partial: '/layouts/partial/cart'),
-      turbo_stream.replace("cart_item_#{cart_item_params[:product_id]}_counter", partial: '/layouts/partial/cart_item_counter', locals: { id: cart_item_params[:product_id] }),
+      turbo_stream.replace(
+        "cart_item_#{cart_item_params[:product_id]}_counter",
+        partial: '/layouts/partial/cart_item_counter', locals: { id: cart_item_params[:product_id] }
+      ),
       success_notice('Товар добавлен в корзину.')
     ] if cart_item.save
 
@@ -18,7 +21,9 @@ class CartItemsController < ApplicationController
 
   # PATCH/PUT /order_items/1 or /order_items/1.json
   def update
-
+    cart      = Cart.includes(:cart_items).find(current_user.cart.id)
+    cart_item = cart.cart_items.find(params[:id])
+    params[:quantity] != "0" ? handle_update_carts(cart_item) : handle_remove_item(cart, cart_item)
   end
 
   # DELETE /order_items/1 or /order_items/1.json
@@ -32,6 +37,30 @@ class CartItemsController < ApplicationController
   end
 
   private
+
+    def handle_update_carts(cart_item)
+      cart_item.update(quantity: params[:quantity])
+      render turbo_stream: [
+        turbo_stream.replace(
+          "cart_item_#{cart_item.id}",
+          partial: '/cart_items/cart_item', locals: { cart_item: cart_item }
+        )
+      ] + update_counters(cart_item.product.id)
+    end
+
+    def handle_remove_item(cart, cart_item)
+      id = cart_item.product.id
+      cart_item.destroy
+      if cart.cart_items.size > 1
+        return render turbo_stream: [ turbo_stream.remove("cart_item_#{cart_item.id}") ] + update_counters(id)
+      end
+
+      render turbo_stream: [
+        turbo_stream.append(:modal, "<script>closeModal();</script>".html_safe),
+        success_notice('Ваша корзина пуста!')
+      ] + update_counters(id)
+    end
+
     # Use callbacks to share common setup or constraints between actions.
     def set_cart_item
       @cart_item = current_user.cart.cart_items.find(params[:id])
@@ -40,5 +69,15 @@ class CartItemsController < ApplicationController
     # Only allow a list of trusted parameters through.
     def cart_item_params
       params.require(:cart_item).permit(:product_id, :quantity)
+    end
+
+    def update_counters(id)
+      [
+        turbo_stream.replace(:cart_logo, partial: '/layouts/partial/cart'),
+        turbo_stream.replace(
+          "cart_item_#{id}_counter",
+          partial: '/layouts/partial/cart_item_counter', locals: { id: id }
+        )
+      ]
     end
 end
