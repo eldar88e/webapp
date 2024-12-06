5cdb814292a60b3d0abc62d2b1d9f5c2f6992fe4
Eldar Eminov <em88el@gmail.com>
2024-12-06 10:39:54 +0300

    Add agree

---
+++ app/javascript/controllers/agreement_controller.js
@@ -0,0 +1,28 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = ["form", "agreement", "check_box"];
+
+  connect() {
+    //this.element.textContent = "Hello World!"
+    console.log(this.element);
+  }
+
+  visible(event) {
+    event.preventDefault();
+    this.agreementTarget.style = "display: block;"
+    this.formTarget.style = "display: none;"
+  }
+
+  agree(event) {
+    event.preventDefault();
+    this.check_boxTarget.checked = true;
+    this.formTarget.style = "display: block;"
+    this.agreementTarget.style = "display: none;"
+  }
+
+  close() {
+    this.formTarget.style = "display: block;"
+    this.agreementTarget.style = "display: none;"
+  }
+}
