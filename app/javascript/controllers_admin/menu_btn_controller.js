a4af6ff62dbfaf48907f8b1b89b0114a699b4d50
Eldar Eminov <em88el@gmail.com>
2024-12-13 23:19:40 +0300

    Update admin styles and add i18n for pagy

--- app/javascript/controllers_admin/menu_btn_controller.js
+++ app/javascript/controllers_admin/menu_btn_controller.js
@@ -1,16 +1,9 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-
-    connect() {
-        console.log(document.getElementById('sidebarBackdrop').style.display);
-    }
-
     showMenu() {
         let sidebarBackdrop = document.getElementById('sidebarBackdrop');
         let sidebar = document.getElementById('sidebar');
-
-        console.log(sidebar.style.display);
 
         if (sidebar.style.display === 'none' && sidebar.style.display === 'none') {
             sidebar.style = "display: block;";
