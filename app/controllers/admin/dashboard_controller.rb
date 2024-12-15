4798b122f90ca292a7b4d95145ff3d2e69e5e6b7
Eldar Eminov <em88el@gmail.com>
2024-12-16 01:18:30 +0300

    Add admin pane grafic

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -10,6 +10,17 @@ module Admin
 
     def index
       redirect_to admin_login_path unless user_signed_in?
+
+      @total_orders = Order.count
+
+      if @total_orders.zero?
+        @percentages = {}
+      else
+        @percentages = Order.statuses.keys.each_with_object({}) do |status, hash|
+          status_count = Order.where(status: Order.statuses[status]).count
+          hash[status.to_sym] = ((status_count.to_f / @total_orders) * 100).round(2)
+        end
+      end
     end
   end
 end
