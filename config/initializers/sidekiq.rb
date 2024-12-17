bf8fa44980ee35b36224453dfa53b86c9560e52f
Eldar Eminov <em88el@gmail.com>
2024-12-17 18:19:08 +0300

    Small fix

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -3,7 +3,7 @@ host = Rails.env.production? ? 'redis' : 'localhost'
 Sidekiq.configure_server do |config|
   config.redis = { url: "redis://#{host}:6379/2" }
 
-  config.on(:startup) { puts 'Sidekiq сервер запущен!' }
+  config.on(:startup) { puts 'Sidekiq server is running!' }
 end
 
 Sidekiq.configure_client do |config|
