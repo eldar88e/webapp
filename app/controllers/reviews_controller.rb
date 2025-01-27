2015650def24756e37d60e72b6567ed8847c3893
Eldar Eminov <em88el@gmail.com>
2025-01-27 20:26:38 +0300

    Fix review_params and delete pry

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -32,9 +32,8 @@ class ReviewsController < ApplicationController
   end
 
   def review_params
-    binding.pry
     permitted_params = params.require(:review).permit(:content, :rating, photos: [])
-    permitted_params[:photos] = [permitted_params[:photos]] if permitted_params[:photos].is_a?(String)
+    permitted_params[:photos] = [params.require(:review)[:photos]] unless params.require(:review)[:photos].is_a?(Array)
     permitted_params
   end
 
