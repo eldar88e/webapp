275a5eda6a5a7db1769b0971fca28651d83a71e4
Eldar Eminov <em88el@gmail.com>
2025-01-29 06:19:05 +0300

    Update config.hosts for prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -112,7 +112,7 @@ Rails.application.configure do
   config.active_record.attributes_for_inspect = [ :id ]
 
   # Enable DNS rebinding protection and other `Host` header attacks.
-  config.hosts = %w[webapp.open-ps.ru strattera.tgapp.online miniapp localhost]
+  config.hosts = %w[webapp.open-ps.ru strattera.tgapp.online staging.tgapp.online miniapp localhost]
   # config.hosts = [
   #   "example.com",     # Allow requests from example.com
   #   /.*\.example\.com/ # Allow requests from subdomains like `www.example.com`
