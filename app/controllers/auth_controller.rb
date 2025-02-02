fbd6c5c98018f27bf5a9e02f831134757a179d9e
Eldar Eminov <em88el@gmail.com>
2025-02-02 14:32:51 +0300

    Refactor

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -13,15 +13,13 @@ class AuthController < ApplicationController
   end
 
   def telegram_auth
-    # unless user_signed_in?
-      data      = params.to_unsafe_h.except(:controller, :action)
-      init_data = URI.decode_www_form(data['initData']).to_h
-      return redirect_to "https://t.me/#{settings[:tg_main_bot]}", allow_other_host: true if init_data.blank?
+    data      = params.to_unsafe_h.except(:controller, :action)
+    init_data = URI.decode_www_form(data['initData']).to_h
+    return redirect_to_telegram if init_data.blank?
 
-      tg_user = JSON.parse init_data['user']
-      user    = User.find_or_create_by_tg(tg_user)
-      sign_in(user)
-    # end
+    tg_user = JSON.parse init_data['user']
+    user    = User.find_or_create_by_tg(tg_user)
+    sign_in(user)
 
     render json: { success: true, user: current_user, params: init_data['start_param'] } # head :ok
   end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,6 @@
 class Order < ApplicationRecord
   ONE_WAIT = 3.hours
+
   belongs_to :user
   has_many :order_items, dependent: :destroy
 
@@ -8,8 +9,7 @@ class Order < ApplicationRecord
 
   enum status: { initialized: 0, unpaid: 1, paid: 2, processing: 3, shipped: 4, cancelled: 5, overdue: 7, refunded: 8 }
 
-  # after_update :check_status_change
-  after_commit :check_status_change #, if: -> { saved_change_to_status? }
+  after_commit :check_status_change # , if: -> { saved_change_to_status? }
 
   def order_items_with_product
     order_items.includes(:product)
@@ -73,140 +73,5 @@ class Order < ApplicationRecord
 
   def check_status_change
     ReportJob.perform_later(order_id: self.id)
-
-    return
-
-    case status
-    when 'unpaid'
-      on_unpaid
-    when 'pending'
-      on_pending
-    when 'processing'
-      on_processing
-    when 'shipped'
-      on_shipped
-    when 'cancelled'
-      on_cancelled
-    when 'refunded'
-      on_refunded
-    when 'overdue'
-      on_overdue
-    end
-  end
-
-  # Логика для статуса "не оплачен"
-  def on_unpaid
-    card = Setting.fetch_value(:card)
-    msg  = "#{I18n.t('tg_msg.unpaid.msg', order: id)}\n\n"
-    msg += I18n.t(
-      'tg_msg.unpaid.main',
-      card: card,
-      price: total_amount,
-      items: order_items_str,
-      address: user.full_address,
-      postal_code: user.postal_code,
-      fio: user.full_name,
-      phone: user.phone_number
-    )
-    ActiveRecord::Base.transaction do
-      TelegramService.delete_msg('', self.user.tg_id, self.msg_id) if self.msg_id
-      msg_id = TelegramService.call(msg, self.user.tg_id, markup: 'i_paid')
-      self.update_columns(msg_id: msg_id)
-      AbandonedOrderReminderJob.set(wait: ONE_WAIT).perform_later(order_id: id, msg_type: :one)
-      Rails.logger.info "Order #{id} is now unpaid"
-    end
-  end
-
-  # Логика для статуса "на этапе проверки платежа"
-  def on_pending
-    msg = I18n.t(
-      'tg_msg.paid_admin',
-      order: id,
-      price: total_amount,
-      items: order_items_str,
-      address: user.full_address,
-      fio: user.full_name,
-      phone: user.phone_number
-    )
-    ActiveRecord::Base.transaction do
-      self.user.cart.destroy
-      TelegramService.delete_msg('', self.user.tg_id, self.msg_id) if self.msg_id
-      TelegramService.call(msg, nil, markup: 'approve_payment') # send to admin
-      msg_id = TelegramService.call(I18n.t('tg_msg.paid_client'), user.tg_id) # send client
-      update_columns(msg_id: msg_id)
-      Rails.logger.info "Order #{id} is now paid"
-    end
-  end
-
-  # Логика для статуса "в обработке"
-  def on_processing
-    msg = I18n.t(
-      'tg_msg.on_processing_courier',
-      order: id,
-      postal_code: user.postal_code,
-      items: order_items_str(true),
-      address: user.full_address,
-      fio: user.full_name,
-      phone: user.phone_number
-    )
-    ActiveRecord::Base.transaction do
-      deduct_stock
-      TelegramService.call(msg, :courier, markup: 'submit_tracking') # send to deliver
-      TelegramService.delete_msg('', user.tg_id, self.msg_id)
-      msg_id = TelegramService.call(I18n.t('tg_msg.on_processing_client', order: id), user.tg_id, markup: 'new_order')
-      update_columns(msg_id: msg_id)
-      Rails.logger.info "Order #{id} is being processed"
-    end
-  end
-
-  def on_shipped
-    # Логика для статуса "отправлен"
-    msg = I18n.t(
-      'tg_msg.on_shipped_courier',
-      order: id,
-      price: total_amount,
-      items: order_items_str,
-      address: user.full_address,
-      fio: user.full_name,
-      phone: user.phone_number,
-      track: tracking_number
-    )
-    TelegramService.delete_msg('', user.tg_id, self.msg_id)
-    msg_id = TelegramService.call(msg, user.tg_id, markup: 'new_order')
-    update_columns(msg_id: msg_id)
-    Rails.logger.info "Order #{id} has been shipped"
-  end
-
-  def on_cancelled
-    # Логика для статуса "отменен"
-    ActiveRecord::Base.transaction do
-      self.order_items.includes(:product).each do |item|
-        product = item.product
-        next unless product
-
-        product.increment!(:stock_quantity, item.quantity)
-      end
-    end
-    Rails.logger.info "Order #{id} has been cancelled"
-    msg = "❌ Заказ #{id} был отменен!\nОстатки были обновлены."
-    TelegramService.call msg
-    TelegramService.delete_msg('', user.tg_id, self.msg_id)
-    TelegramService.call(I18n.t('tg_msg.cancel', order: id), user.tg_id, markup: 'new_order')
-  end
-
-  def on_refunded
-    # Логика для статуса "возвращен"
-    msg = "Order #{id} has been refunded"
-    Rails.logger.info msg
-    TelegramService.call msg # TODO: шлет уведомление только админу
-  end
-
-  def on_overdue
-    # Логика для статуса "просрочен"
-    Rails.logger.info "Order #{id} has been overdue"
-    TelegramService.delete_msg('', user.tg_id, self.msg_id)
-    msg    = I18n.t('tg_msg.unpaid.reminder.overdue', order: id)
-    msg_id = TelegramService.call(msg, user.tg_id, markup: 'new_order')
-    update_columns(msg_id: msg_id)
   end
 end
