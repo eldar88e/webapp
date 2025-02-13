62485a10de59132e38e18e1074e5fc8d79c7550e
Eldar Eminov <em88el@gmail.com>
2025-02-13 19:00:55 +0300

    Fix total_quantity_sold

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -7,6 +7,7 @@ class OrderItem < ApplicationRecord
 
   def self.total_quantity_sold(start_date, end_date, group_by)
     joins(:order)
+      .where.not(product_id: Setting.fetch_value(:delivery_id))
       .where(orders: { updated_at: start_date..end_date, status: :shipped })
       .group(group_by)
       .sum(:quantity)
