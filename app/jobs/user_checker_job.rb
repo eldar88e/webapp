117ca6b7ee4e6e6bae613c656e0b9f2e9347ba0e
Eldar Eminov <em88el@gmail.com>
2025-02-16 16:12:02 +0300

    Add update_user_status method and Refactor

--- app/jobs/user_checker_job.rb
+++ app/jobs/user_checker_job.rb
@@ -5,11 +5,24 @@ class UserCheckerJob < ApplicationJob
     user   = User.find(id)
     msg    = "🎉 Добро пожаловать на #{Setting.fetch_value(:app_name)}!"
     result = TelegramService.call(msg, user.tg_id)
-    return unless result.instance_of?(Integer)
 
-    user.update(started: true, is_blocked: false)
-    msg = "User #{user.id} started bot"
-    Rails.logger.info msg
-    TelegramJob.perform_later(msg: msg, id: Setting.fetch_value(:test_id))
+    if result.instance_of?(Integer)
+      user.update(started: true, is_blocked: false)
+      msg = "User #{user.id} started bot"
+      Rails.logger.info msg
+      TelegramJob.perform_later(msg: msg, id: Setting.fetch_value(:test_id))
+    else
+      update_user_status(result, user)
+    end
+  end
+
+  private
+
+  def update_user_status(error, user)
+    if error.message.include?('chat not found')
+      user.update(started: false)
+    elsif error.message.include?('bot was blocked')
+      user.update(is_blocked: true)
+    end
   end
 end
