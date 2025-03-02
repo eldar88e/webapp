2d4210b399c983cbbdb25c6eacf099e13f953932
Eldar Eminov <em88el@gmail.com>
2025-03-02 18:41:02 +0300

    Update SendReviewRequestJob and unlock_user_privileges

--- app/jobs/send_review_request_job.rb
+++ app/jobs/send_review_request_job.rb
@@ -4,12 +4,22 @@ class SendReviewRequestJob < ApplicationJob
   def perform(**args)
     product = Product.find(args[:product_id])
     user    = User.find(args[:user_id])
-    order   = Order.find(args[:order_id])
-    return if order.status != 'shipped'
+    order   = user.orders.find_by(id: args[:order_id])
+    return if order&.status != 'shipped'
 
-    msg = I18n.t('tg_msg.review', product: product.name)
-    url = "products_#{product.id}_reviews_new"
-    TelegramService.call(msg, user.tg_id, markup_text: 'Оставить отзыв', markup_url: url)
-    Rails.logger.info "Review request sent to user #{user.id} for product #{product.id}"
+    send_review_request(user, product)
+  end
+
+  private
+
+  def send_review_request(user, product)
+    msg    = I18n.t('tg_msg.review', product: product.name)
+    url    = "products_#{product.id}_reviews_new"
+    msg_id = TelegramService.call(msg, user.tg_id, markup_text: 'Оставить отзыв', markup_url: url)
+    if msg_id.instance_of?(Integer)
+      Rails.logger.info "Review request sent to user #{user.id} for product #{product.id}"
+    else
+      limit_user_privileges(msg_id, user)
+    end
   end
 end
