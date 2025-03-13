d685102370bb66fd342991f929f09d73c37d70b6
Eldar Eminov <em88el@gmail.com>
2025-03-14 00:39:04 +0300

    Style: fix avatar

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,5 +9,6 @@ module.exports = {
     'text-blue-600', 'text-red-600', 'text-green-600', 'text-indigo-600', 'text-purple-600',
     'bg-blue-600', 'bg-red-600', 'bg-green-600', 'bg-indigo-600', 'bg-purple-600',
     'bg-red-800', 'bg-blue-800', 'bg-green-800', 'bg-indigo-800', 'bg-purple-800',
+    'w-14', 'h-14'
   ],
 }
