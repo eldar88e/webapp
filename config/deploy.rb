9375f78265985d93183ae547775187f2b194ffe5
Eldar Eminov <em88el@gmail.com>
2025-03-25 17:51:13 +0300

    Chore(deploy): update deploy comand

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,9 +4,9 @@ include SSHKit::DSL
 
 SSHKit.config.output_verbosity = :debug
 
-# Объявляем глобальные переменные (с префиксом `$`)
 $server = 'deploy@staging.tgapp.online'
-$app_path = '/home/deploy/miniapp_staging'
+$app_name = 'miniapp_staging'
+$app_path = "/home/deploy/#{$app_name}"
 $docker_compose_file = 'docker-compose.staging.yml'
 $rails_service = 's-miniapp'
 
@@ -14,14 +14,26 @@ def update
   on $server do
     within $app_path do
       execute :git, 'pull'
+      execute :docker, "compose -f #{$docker_compose_file} exec #{$rails_service} bundle exec rails db:prepare"
+      execute :docker, "compose -f #{$docker_compose_file} exec #{$rails_service} bundle exec rails assets:precompile"
       execute :docker, "compose -f #{$docker_compose_file} exec #{$rails_service} bundle exec rails restart"
     end
   end
 end
 
-def restart_server
+def restart
   on $server do
     within $app_path do
+      execute :docker, "compose -f #{$docker_compose_file} up --build #{$rails_service} sidekiq"
+    end
+  end
+end
+
+def rebuild
+  on $server do
+    within $app_path do
+      execute :docker, "compose -f #{$docker_compose_file} down #{$rails_service} sidekiq"
+      execute :docker, "volume rm #{$app_name}_gems"
       execute :docker, "compose -f #{$docker_compose_file} up --build #{$rails_service} sidekiq"
     end
   end
@@ -32,6 +44,8 @@ task_name = ARGV[0]
 case task_name
 when 'update'
   update
-when 'restart_server'
-  restart_server
+when 'restart'
+  restart
+when 'rebuild'
+  rebuild
 end
