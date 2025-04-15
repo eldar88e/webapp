28d6f830dea6056d63df849f0ac9430f4c917671
Eldar Eminov <em88el@gmail.com>
2025-04-15 06:04:06 +0300

    Fix(ahoy): add geocode method

--- config/initializers/ahoy.rb
+++ config/initializers/ahoy.rb
@@ -1,5 +1,21 @@
 module Ahoy
-  class Store < Ahoy::DatabaseStore; end
+  class Store < Ahoy::DatabaseStore
+    def geocode(visit)
+      db     = MaxMindDB.new(Rails.root.join('lib/data/GeoLite2-City.mmdb'))
+      result = db.lookup(visit.ip)
+
+      {
+        country: result.country.name,
+        region: result.subdivisions.first&.name,
+        city: result.city.name,
+        latitude: result.location.latitude,
+        longitude: result.location.longitude
+      }
+    rescue StandardError => e
+      Rails.logger.error "Ahoy geocoding failed: #{e.message}"
+      {}
+    end
+  end
 end
 
 Ahoy.api = false
