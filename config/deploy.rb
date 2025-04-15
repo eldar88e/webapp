d72aef5632d597bcc7f2c2b1bef00051cfee0bf8
Eldar Eminov <em88el@gmail.com>
2025-04-15 09:16:21 +0300

    Chore(sidekiq): add queues ahoy

--- config/deploy.rb
+++ config/deploy.rb
@@ -57,8 +57,8 @@ def rebuild
       execute :git, 'pull'
       execute :docker, "compose -f #{$docker_compose_file} build #{$rails_service} sidekiq"
       execute :docker, "compose -f #{$docker_compose_file} down #{$rails_service} sidekiq base"
-      execute :docker, "volume rm #{$app_name}_gems"
-      execute :docker, "compose -f #{$docker_compose_file} up #{$rails_service} sidekiq"
+      execute :docker, "volume rm #{$app_name}_gems || true"
+      execute :docker, "compose -f #{$docker_compose_file} up --build #{$rails_service} sidekiq"
       # execute :docker, "compose -f #{$docker_compose_file} exec #{$rails_service} bundle exec rails db:prepare"
       # execute :docker, "compose -f #{$docker_compose_file} exec #{$rails_service} yarn vite build"
       # execute :docker, "compose -f #{$docker_compose_file} exec #{$rails_service} bundle exec rails restart"
