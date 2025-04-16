c1ae64f17e2a553af002d51ffa0e2a4d51900c95
Eldar Eminov <em88el@gmail.com>
2025-04-16 08:49:06 +0300

    Chore(mailer): add custom_devise_mailer

---
+++ app/mailers/custom_devise_mailer.rb
@@ -0,0 +1,9 @@
+class CustomDeviseMailer < Devise::Mailer
+  helper :application
+  include Devise::Controllers::UrlHelpers
+  default template_path: 'devise/mailer'
+
+  def confirmation_instructions(record, token, opts = {})
+    # nothing not send because email is generated with system
+  end
+end
