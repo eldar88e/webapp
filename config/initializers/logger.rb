297b4e59917727b728961a55ac9cb3b8ea0dd1fb
Eldar Eminov <em88el@gmail.com>
2025-04-27 12:01:16 +0300

    Chore(log): update logging

--- config/initializers/logger.rb
+++ config/initializers/logger.rb
@@ -1,39 +1,46 @@
-if Rails.env.production?
-  file_logger = ActiveSupport::Logger.new("log/#{Rails.env}.log", 10, 50.megabytes)
-  file_logger.level = Logger::INFO
-  file_logger.formatter = proc do |severity, _timestamp, progname, message|
-    result = {
-      timestamp: Time.current,
-      level: severity,
-      progname: progname || 'rails',
-      message: message
-    }
-    if defined?(Sidekiq::CLI)
-      result[:progname] = 'sidekiq'
-      if message.is_a?(String)
-        msg = message.dup
-        msg.delete_prefix!('[ActiveJob] ')
-        job_name = msg[/\[([\w|:]+Job)\]/, 1]
-        msg.gsub!(job_name, '') if job_name.present?
-        result[:job_name] = job_name if job_name.present?
-        job_id = msg[/\[(\w+-\w+-\w+-\w+-\w+)\]/, 1]
-        msg.gsub!(job_id, '') if job_id.present?
-        result[:job_id] = job_id if job_id.present?
-        result[:message] = msg.gsub(/\[\]|\(Job ID: \)/, '').strip.squeeze(' ')
-      end
-    end
-    "#{result.to_json}\n"
-  end
+file_logger = ActiveSupport::Logger.new("log/#{Rails.env}.log", 10, 50.megabytes)
+file_logger.level = Logger::INFO
+file_logger.formatter = proc do |severity, _timestamp, progname, message|
+  result = {
+    timestamp: Time.current,
+    level: severity,
+    progname: progname || 'rails'
+  }
 
-  # Rails.logger = ActiveSupport::TaggedLogging.new(file_logger)
-
-  if defined?(Rails::Console)
-    base_logger = ActiveSupport::Logger.new($stdout)
-    logger      = ActiveSupport::BroadcastLogger.new(base_logger)
-    logger.broadcast_to(file_logger)
+  case message
+  when String
+    result[:message] = message
+  when Exception
+    result[:message] = message.message
+    result[:error_class] = message.class.name
+    result[:backtrace] = (message.backtrace || []).take(10) # можно взять только первые 10 строк
   else
-    logger = file_logger
+    result[:message] = message.inspect
   end
 
-  Rails.logger = ActiveSupport::TaggedLogging.new(logger)
+  if defined?(Sidekiq::CLI)
+    result[:progname] = 'sidekiq'
+    if message.is_a?(String)
+      msg = message.dup
+      msg.delete_prefix!('[ActiveJob] ')
+      job_name = msg[/\[([\w|:]+Job)\]/, 1]
+      msg.gsub!(job_name, '') if job_name.present?
+      result[:job_name] = job_name if job_name.present?
+      job_id = msg[/\[(\w+-\w+-\w+-\w+-\w+)\]/, 1]
+      msg.gsub!(job_id, '') if job_id.present?
+      result[:job_id] = job_id if job_id.present?
+      result[:message] = msg.gsub(/\[\]|\(Job ID: \)/, '').strip.squeeze(' ')
+    end
+  end
+  "#{result.to_json}\n"
 end
+
+if Rails.const_defined?(:Console) || Rails.env.development?
+  base_logger   = ActiveSupport::Logger.new($stdout)
+  custom_logger = ActiveSupport::BroadcastLogger.new(base_logger)
+  custom_logger.broadcast_to(file_logger)
+else
+  custom_logger = file_logger
+end
+
+Rails.logger = ActiveSupport::TaggedLogging.new(custom_logger)
