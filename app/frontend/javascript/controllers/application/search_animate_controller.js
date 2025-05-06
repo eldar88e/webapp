7db1e176313271d7b32e1fc15180dc2296d4f6be
Eldar Eminov <em88el@gmail.com>
2025-05-07 00:46:21 +0300

    Fix(search): hiden placeholder when focus

--- app/frontend/javascript/controllers/application/search_animate_controller.js
+++ app/frontend/javascript/controllers/application/search_animate_controller.js
@@ -6,35 +6,49 @@ export default class extends Controller {
     }
 
     connect() {
+        this.start()
+    }
+
+    start() {
         this.currentWordIndex = 0
         this.currentCharIndex = 0
         this.typing = true
         this.cursorVisible = true
+        this.stopped = false
         this.startTyping()
         this.toggleCursor()
     }
 
+    stop() {
+        this.stopped = true
+        clearTimeout(this.typingTimeout)
+        clearTimeout(this.cursorTimeout)
+        this.element.placeholder = '';
+    }
+
     startTyping() {
+        if (this.stopped) return
+
         const word = this.wordsValue[this.currentWordIndex].substring(0, 33);
 
         if (this.typing) {
             if (this.currentCharIndex <= word.length) {
                 this.updateText(word.substring(0, this.currentCharIndex))
                 this.currentCharIndex++
-                setTimeout(() => this.startTyping(), 150)
+                this.typingTimeout = setTimeout(() => this.startTyping(), 200)
             } else {
                 this.typing = false
-                setTimeout(() => this.startTyping(), 10)
+                this.typingTimeout = setTimeout(() => this.startTyping(), 10)
             }
         } else {
             if (this.currentCharIndex >= 0) {
                 this.updateText(word.substring(0, this.currentCharIndex))
                 this.currentCharIndex--
-                setTimeout(() => this.startTyping(), 150)
+                this.typingTimeout = setTimeout(() => this.startTyping(), 150)
             } else {
                 this.typing = true
                 this.currentWordIndex = (this.currentWordIndex + 1) % this.wordsValue.length
-                setTimeout(() => this.startTyping(), 10)
+                this.typingTimeout = setTimeout(() => this.startTyping(), 10)
             }
         }
     }
@@ -44,11 +58,11 @@ export default class extends Controller {
     }
 
     toggleCursor() {
+        if (this.stopped) return
+
         this.cursorVisible = !this.cursorVisible
-        setTimeout(() => {
-            const currentText = this.element.placeholder.replace(/\|$/, '')
-            this.updateText(currentText)
-            this.toggleCursor()
-        }, 300)
+        const currentText = this.element.placeholder.replace(/\|$/, '')
+        this.updateText(currentText)
+        this.cursorTimeout = setTimeout(() => this.toggleCursor(), 300)
     }
 }
