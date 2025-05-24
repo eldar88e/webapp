bafbd14a142beeef22bd116e13d0eaf2843b5d3e
Eldar Eminov <em88el@gmail.com>
2025-05-24 22:13:35 +0300

    Feature(orders): add new api data

--- app/controllers/api/v1/orders_controller.rb
+++ app/controllers/api/v1/orders_controller.rb
@@ -2,8 +2,17 @@ module Api
   module V1
     class OrdersController < Api::V1::BaseController
       def index
-        @orders = Order.all
-        render json: @orders
+        @orders = Order.includes(:user, { order_items: [:product] }, :bank_card).order(created_at: :desc)
+        render json: @orders, include: {
+          user: { only: [:id, :address, :phone, :first_name, :middle_name, :last_name] },
+          order_items: {
+            only: [:quantity, :price],
+            include: {
+              product: { only: [:name] }
+            }
+          },
+          bank_card: { only: [:name, :fio] }
+        }, only: [:id, :status, :created_at, :shipped_at, :paid_at, :total_amount]
       end
     end
   end
