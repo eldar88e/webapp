ca4ef1fcb16fbf4e63708fc329617be0ee51224c
Eldar Eminov <em88el@gmail.com>
2025-08-31 03:20:45 +0300

    Fix(sw): add register for serviceWorker

--- app/frontend/javascript/application.js
+++ app/frontend/javascript/application.js
@@ -53,3 +53,16 @@ window.rightModal = function () {
 window.closeMiniApp = function () {
   Telegram.WebApp.close();
 };
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", function () {
+    navigator.serviceWorker
+      .register("/service-worker.js")
+      .then(function () {
+        // ...
+      })
+      .catch(function (error) {
+        console.log("Error registering ServiceWorker:", error);
+      });
+  });
+}
