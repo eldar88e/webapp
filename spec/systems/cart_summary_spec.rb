d9d272ba00709fbcddd941487a47d73f2fb15131
Eldar Eminov <em88el@gmail.com>
2025-08-31 03:45:26 +0300

    Test(spec): add wait for cart_summary

--- spec/systems/cart_summary_spec.rb
+++ spec/systems/cart_summary_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe 'CartSummary', type: :system do
   let(:product_two) { Product.find(4) }
 
   before do
-    driven_by(:selenium_headless) # selenium_chrome rack_test selenium_headless
+    driven_by(:selenium_chrome) # selenium_chrome rack_test selenium_headless
     page.driver.browser.manage.window.resize_to(430, 932) # iphone 14 pro max
     login_as users(:customer), scope: :user
     visit root_path
@@ -63,6 +63,7 @@ RSpec.describe 'CartSummary', type: :system do
   end
 
   it 'have two category' do
+    expect(page).to have_selector('.catalog-nav', wait: 5)
     within('.catalog-nav') do
       expect(find('a.active')).to have_content("#{product.parent.name}")
       expect(all('a').size).to eq(Product.find(Setting.fetch_value(:root_product_id)).children.size)
