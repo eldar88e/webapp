e8fc4693d2ac42958843e60858fcbd043dbc35cc
Eldar Eminov <em88el@gmail.com>
2025-09-09 03:30:33 +0300

    Refactor(js): purchase_items controller

--- app/frontend/javascript/controllers/admin/purchase_items_controller.js
+++ app/frontend/javascript/controllers/admin/purchase_items_controller.js
@@ -1,10 +1,13 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  static targets = ["items", "template"]
+  static targets = ["items", "template"];
 
   add() {
-    const content = this.templateTarget.innerHTML.replace(/NEW_RECORD/g, new Date().getTime());
+    const content = this.templateTarget.innerHTML.replace(
+      /NEW_RECORD/g,
+      new Date().getTime(),
+    );
     this.itemsTarget.insertAdjacentHTML("beforeend", content);
   }
 
