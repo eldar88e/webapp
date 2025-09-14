597ca23593a1d6c8037e060d3df89afd0da48988
Eldar Eminov <em88el@gmail.com>
2025-09-15 02:49:31 +0300

    Chore(service): add PurchasePlannerService and change storage to :beget

---
+++ app/services/purchase_planner_service.rb
@@ -0,0 +1,27 @@
+class PurchasePlannerService
+  STATISTICS_PERIOD = 1.year
+  LEAD_TIME = 20.days
+
+  def initialize(product)
+    @product = product
+  end
+
+  def avg_daily_consumption(period = STATISTICS_PERIOD)
+    one_year_orders = OrderItem.where(product_id: @product.id, created_at: period.ago..Time.current)
+    total = one_year_orders.sum(:quantity)
+    total / (period / 1.day).to_f
+  end
+
+  def expected_finish_date
+    return nil if avg_daily_consumption.zero?
+
+    days_left = @product.stock_quantity / avg_daily_consumption
+    Time.zone.today + days_left.round
+  end
+
+  def purchase_date(lead_time = LEAD_TIME)
+    return nil unless expected_finish_date
+
+    expected_finish_date - lead_time
+  end
+end
