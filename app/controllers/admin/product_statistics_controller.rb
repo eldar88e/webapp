656d690d602cf7f8b7378098f4e462fd6a0b630c
Eldar Eminov <em88el@gmail.com>
2025-09-19 19:41:23 +0300

    Feature(controller): add default date_range for ProductStatistics

--- app/controllers/admin/product_statistics_controller.rb
+++ app/controllers/admin/product_statistics_controller.rb
@@ -1,5 +1,7 @@
 module Admin
   class ProductStatisticsController < Admin::ApplicationController
+    before_action :set_date_range
+
     def index
       root_products = Product.available.where(ancestry: nil).order(:created_at)
       ids = root_products.map { |item| item.descendants.ids }.flatten
@@ -7,6 +9,13 @@ module Admin
                         .where(id: ids, price: 0..)
                         .order(:name)
       @product_statistics = StatisticsService.call(products)
+    end
+
+    private
+
+    def set_date_range
+      @start_date = params[:start_date].present? ? params[:start_date].to_date : 6.months.ago
+      @end_date = params[:end_date].present? ? params[:end_date].to_date : Time.current
     end
   end
 end
