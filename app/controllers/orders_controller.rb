a4adfa3d4d1936fe80a21cfb61a5af7c8bd16906
Eldar Eminov <em88el@gmail.com>
2025-09-28 18:59:24 +0300

    Fix(cart): when submit not work redirect

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -25,16 +25,19 @@ class OrdersController < ApplicationController
     service = CreateOrderService.call(current_user, params[:user][:bonus])
     return handle_success_notice if service[:success]
 
-    redirect_to root_path, alert: service[:error]
+    # redirect_to root_path, alert: service[:error]
+    render turbo_stream: [
+      send_notice(service[:error], 'danger'),
+      turbo_stream.append(:modal, '<script>window.location.href = "/";</script>'.html_safe),
+    ], status: :unprocessable_entity
   end
 
   def handle_success_notice
-    # render turbo_stream: [
-    #   success_notice(t('.success')),
-    #   turbo_stream.append(:modal, '<script>window.location.href = "/";</script>'.html_safe),
-    #   turbo_stream.append(:modal, '<script>closeMiniApp();</script>'.html_safe)
-    # ]
-    redirect_to root_path, notice: t('.success')
+    render turbo_stream: [
+      success_notice(t('.success')),
+      turbo_stream.append(:modal, '<script>window.location.href = "/";</script>'.html_safe),
+      turbo_stream.append(:modal, '<script>closeMiniApp();</script>'.html_safe)
+    ]
   end
 
   def update_user
