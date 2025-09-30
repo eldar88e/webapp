d779f78b920f3e23e73a834561e87a4089fb5694
Eldar Eminov <em88el@gmail.com>
2025-09-30 16:01:20 +0300

    Test(cart): up wait time

--- spec/systems/cart_summary_spec.rb
+++ spec/systems/cart_summary_spec.rb
@@ -14,7 +14,7 @@ RSpec.describe 'CartSummary', type: :system do
 
   describe 'when buy products' do
     before do
-      find("#cart-btn-#{product.id}", wait: 10).click_button
+      find("#cart-btn-#{product.id}", wait: 20).click_button
     end
 
     it "have one product in cart" do
