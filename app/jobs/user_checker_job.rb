9a8effa88de298b5fb019e4c111529276c280532
Eldar Eminov <em88el@gmail.com>
2025-10-08 15:57:40 +0300

    Feature(job): add notice for admin and add markup for user message

--- app/jobs/user_checker_job.rb
+++ app/jobs/user_checker_job.rb
@@ -4,8 +4,14 @@ class UserCheckerJob < ApplicationJob
   def perform(id)
     user   = User.find(id)
     msg    = "🎉 Добро пожаловать #{user.first_name || user.first_name_raw} на #{Setting.fetch_value(:app_name)}!"
-    result = TelegramService.call(msg, user.tg_id)
-    return user.update(started: true, is_blocked: false) if result.instance_of?(Integer)
+    result = TelegramService.call(msg, user.tg_id, markup: 'mailing')
+    if result.instance_of?(Integer)
+      user.update(started: true, is_blocked: false)
+      msg = "User #{user.id} started bot with webapp"
+      Rails.logger.info msg
+      TelegramJob.perform_later(msg: msg, id: Setting.fetch_value(:test_id))
+      # TODO: убрать со временем уведомление админа
+    end
 
     limit_user_privileges(result, user)
   end
