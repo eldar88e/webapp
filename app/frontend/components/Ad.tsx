aededcb62fcfd131218a7464ed936a17cffab0d5
Eldar Eminov <em88el@gmail.com>
2025-10-21 15:40:42 +0300

    Chore(component): update ad

--- app/frontend/components/Ad.tsx
+++ app/frontend/components/Ad.tsx
@@ -1,24 +1,28 @@
 import React, { useEffect, useState, useRef } from "react";
 
-export default function Ad({ item }: { item: String }) {
+export default function Ad({ item, url }: { item: string, url: string }) {
   const [visible, setVisible] = useState(false);
   const adRef = useRef<HTMLDivElement | null>(null);
 
-  if (!item) return null;
+  if (!item || !url) return null;
 
   useEffect(() => {
+    let ticking = false;
+
     const handleScroll = () => {
       if (!adRef.current) return;
+      if (ticking) return;
 
-      const height = adRef.current.offsetHeight;
-      const threshold = height + 10; // высота блока + 10px
-      const scrollTop = window.scrollY || document.documentElement.scrollTop;
+      window.requestAnimationFrame(() => {
+        const height = adRef.current!.offsetHeight;
+        const threshold = height + 10;
+        const scrollTop = window.scrollY || document.documentElement.scrollTop;
 
-      if (scrollTop > threshold) {
-        setVisible(true);
-      } else {
-        setVisible(false);
-      }
+        setVisible(scrollTop > threshold);
+        ticking = false;
+      });
+
+      ticking = true;
     };
 
     window.addEventListener("scroll", handleScroll);
@@ -26,14 +30,19 @@ export default function Ad({ item }: { item: String }) {
   }, []);
 
   return (
-    <div
-      ref={adRef}
-      className={`fixed top-0 left-0 w-full text-white transition-transform duration-500 ease-in-out z-50 ${
-        visible ? "translate-y-0" : "-translate-y-full"
-      }`}
-      style={{ backgroundColor: "#48C928" }}
-    >
-      <div className="container mx-auto px-5 py-3">{item}</div>
-    </div>
+    <a href={url} target="_blank" rel="noopener noreferrer">
+      <div
+        ref={adRef}
+        className={`fixed top-0 left-0 w-full text-white transition-transform transition-opacity duration-500 ease-in-out z-50 ${
+          visible ? "translate-y-0 opacity-100" : "-translate-y-full opacity-0"
+        }`}
+        style={{ backgroundColor: "#48C928" }}
+      >
+        <div
+          className="text-xs container mx-auto px-3 py-2"
+          style={{ lineHeight: "1.2" }}
+        >{item}</div>
+      </div>
+    </a>
   );
 }
