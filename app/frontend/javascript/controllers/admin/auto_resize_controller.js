97863f7971ab9076ff02454e8edadad16bddd7c9
Eldar Eminov <em88el@gmail.com>
2025-12-21 13:46:29 +0300

    Fix(admin): messanger

---
+++ app/frontend/javascript/controllers/admin/auto_resize_controller.js
@@ -0,0 +1,26 @@
+import { Controller } from "@hotwired/stimulus";
+
+export default class extends Controller {
+
+  connect() {
+    this.lineHeight = parseFloat(
+      window.getComputedStyle(this.element).lineHeight
+    )
+    this.maxLines = 5;
+    this.maxHeight = this.lineHeight * this.maxLines;
+  }
+
+  resize() {
+    const textarea = this.element;
+
+    textarea.style.height = "auto";
+
+    if (textarea.scrollHeight > this.maxHeight) {
+      textarea.style.height = `${this.maxHeight}px`;
+      textarea.style.overflowY = "auto";
+    } else {
+      textarea.style.height = `${textarea.scrollHeight}px`;
+      textarea.style.overflowY = "hidden";
+    }
+  }
+}
