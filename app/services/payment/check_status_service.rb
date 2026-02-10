17b4ff4674ca91a3ef8c577f0e918c349061b360
Eldar Eminov <em88el@gmail.com>
2026-02-11 02:00:25 +0300

    Small fix

--- app/services/payment/check_status_service.rb
+++ app/services/payment/check_status_service.rb
@@ -67,7 +67,7 @@ module Payment
 
     def schedule_next_check
       puts '*' * 30
-      puts "Status: #{status}"
+      puts "Status: #{@status}"
       puts '=' * 30
       Payment::CheckStatusJob.set(wait: 15.seconds).perform_later(@transaction.id, @status)
     end
