5aa3450eaf4d269575cae34f27d1871f5af56648
Eldar Eminov <em88el@gmail.com>
2026-02-12 16:39:14 +0300

    Small fix

--- app/jobs/payment/reminder_job.rb
+++ app/jobs/payment/reminder_job.rb
@@ -3,7 +3,9 @@ module Payment
     queue_as :default
 
     def perform(**args)
-      Payment::ReminderService.call(**args)
+      Rails.cache.fetch("payment_reminder_#{args[:order_id]}_#{args[:msg_type]}", expires_in: 30.minutes) do
+        Payment::ReminderService.call(**args)
+      end
     end
   end
 end
