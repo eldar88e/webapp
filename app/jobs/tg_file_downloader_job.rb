097a1ae61b9bd066934ccb6d2215e22bca04882f
Eldar Eminov <em88el@gmail.com>
2026-02-15 18:11:51 +0300

    Small fix

--- app/jobs/tg_file_downloader_job.rb
+++ app/jobs/tg_file_downloader_job.rb
@@ -7,13 +7,13 @@ class TgFileDownloaderJob < ApplicationJob
     return if user.blank? || (response.blank? && args[:msg].blank?)
 
     file_path = response.presence&.file_path
-    file_url  = "https://api.telegram.org/file/bot#{Setting.fetch_value(:tg_token)}/#{file_path}"
-    process_file(file_url, args, user, file_path)
+    process_file(user, file_path, args)
   end
 
   private
 
-  def process_file(file_url, args, user, file_path)
+  def process_file(user, file_path, args)
+    file_url  = "https://api.telegram.org/file/bot#{Setting.fetch_value(:tg_token)}/#{file_path}"
     tempfile  = FileDownloaderService.call(file_url)
     file_type = Marcel::MimeType.for(tempfile)
     name      = file_path.split('/')&.last
@@ -21,6 +21,7 @@ class TgFileDownloaderJob < ApplicationJob
     tg_file   = find_or_create_tg_file(file_hash, args[:file_id], file_type, name)
     attach_file(tg_file, name, file_type, tempfile) unless tg_file.attachment.attached?
     save_message(user, args, tg_file)
+    attach_pdf_to_order(user, tg_file, file_type)
   end
 
   def save_message(user, args, tg_file)
@@ -44,6 +45,18 @@ class TgFileDownloaderJob < ApplicationJob
       response = bot.api.get_file(file_id: args[:file_id])
     end
     response
+  end
+
+  def attach_pdf_to_order(user, tg_file, file_type)
+    return unless file_type == 'application/pdf'
+
+    order = user.orders.paid.order(created_at: :desc).first
+    return if order.blank?
+
+    order.attachment.attach(tg_file.attachment.blob)
+    TelegramJob.perform_later(msg: "PDF прикреплён к заказу №#{order.id}", id: user.tg_id)
+    msg = "Пользователь ##{user.id} прикрепил PDF к заказу №#{order.id}"
+    TelegramJob.perform_later(msg: msg, id: Setting.fetch_value(:admin_ids))
   end
 
   def attach_file(media_file, name, file_type, tempfile)
