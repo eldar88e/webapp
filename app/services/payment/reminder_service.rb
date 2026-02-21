162225fa667d42bb17bdb00daf7cdb97c870274c
Eldar Eminov <em88el@gmail.com>
2026-02-21 20:27:21 +0300

    Small fix

--- app/services/payment/reminder_service.rb
+++ app/services/payment/reminder_service.rb
@@ -15,7 +15,7 @@ module Payment
     end
 
     def send_reminder
-      return if @transaction.order.bank_card.present? || @order.status != 'unpaid'
+      return if @order.bank_card.present? || @order.status != 'unpaid'
       return if @msg_type == 'exit'
 
       remove_old_msg
