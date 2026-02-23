538059edf06ebed991a7bc2ae30917b0b4f6e7b3
Eldar Eminov <em88el@gmail.com>
2026-02-24 00:51:40 +0300

    Small fix

--- app/jobs/user_checker_job.rb
+++ app/jobs/user_checker_job.rb
@@ -2,18 +2,13 @@ class UserCheckerJob < ApplicationJob
   queue_as :default
 
   def perform(id)
-    user   = User.find(id)
-    msg    = "🎉 Добро пожаловать #{user.first_name || user.first_name_raw} на #{Setting.fetch_value(:app_name)}!"
-    result = TelegramService.call(msg, user.tg_id, markup: 'first_msg')
-    return unblock_user(user) if result.instance_of?(Integer)
+    user = User.find(id)
+    name = user.first_name || user.first_name_raw
+    msg  = "🎉 Добро пожаловать #{name} на #{Setting.fetch_value(:app_name).capitalize}!"
+    # result = TelegramService.call(msg, user.tg_id, markup: 'first_msg')
+    user.messages.create(text: msg, is_incoming: false, data: { markup: { markup: 'first_msg' } })
+    return user.update(started: true, is_blocked: false) if result.instance_of?(Integer)
 
     limit_user_privileges(result, user)
-  end
-
-  private
-
-  def unblock_user(user)
-    user.update(started: true, is_blocked: false)
-    Rails.logger.info "User #{user.id} started bot with webapp"
   end
 end
